6efa34fabf6f7bdb4fe16301257e284b7891c508
Denise Mignoli <denisemignoli@gmail.com>
2021-07-15 16:53:30 -0300

    Atualização GetMapping main

--- src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
+++ src/main/java/org/generation/blogPessoal/BlogPessoalApplication.java
@@ -3,6 +3,7 @@ package org.generation.blogPessoal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/")
 public class BlogPessoalApplication {
 
+	@GetMapping
 	public ModelAndView SwaggerUi() {
 		return new ModelAndView("redirect:/swagger-ui/");
 	}
